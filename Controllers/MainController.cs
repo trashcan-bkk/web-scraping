291c858979c03884fd427fd50a7b332792d4281c
panitarn <panitarn.work@gmail.com>
2023-01-18 13:51:49 +0700

    Add the export to excel functionality

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 
@@ -47,9 +48,38 @@ namespace web_scraper.Controllers
             return dataList;
         }
 
+
+        // Export to excel functionality
         [HttpGet("web-scraping/export")]
-        public void Export() 
+        public async Task<FileContentResult> Export(string source, string tagString, string classString) 
         {
+            List<string> dataList = new List<string>();
+            dataList = await Main(source, tagString, classString);
+
+            using (var workbook = new XLWorkbook()) {
+                // Tab
+                var worksheet = workbook.Worksheets.Add("Data");
+                var currentRow = 1;
+
+                //Header
+                worksheet.Cell(currentRow, 1).Value = "Data";
+
+                //Body
+                foreach (var data in dataList) {
+                    currentRow++;
+                    worksheet.Cell(currentRow, 1).Value = data;
+                }
+
+                using (var stream = new MemoryStream()) {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(
+                        content,"application/vnd.openxmlformat-officedocument.spreadsheetml.sheet",
+                        "File.xlsx"
+                    );
+                }
+            }
         }
     }
 }
